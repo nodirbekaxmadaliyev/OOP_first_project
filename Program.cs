f2f1d263d7bd42cd95644b2a700a2097f106874f
Nodirbek Axmadaliyev <nodirbekaxmadaliyev4888@gmail.com>
2024-04-23 21:51:50 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Talaba talaba = new Talaba();
+        Console.Write("Talabaning Id sini kiriting : ");
+        talaba.Id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Talabaning ismini sini kiriting : ");
+        talaba.Name = Console.ReadLine();
+
+        Console.WriteLine($"\nTalabaning Idsi : {talaba.Id} va ismi : {talaba.Name}");
+
+    }
+}
+public class Talaba
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
